b17816a82574c2a14ec3603032f9e7b3b8aab457
Daina <79340025+dazeste@users.noreply.github.com>
2023-04-18 17:02:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const countBtn = document.getElementById("count-btn");
 const homeCount = document.getElementById("home-count");
 const guestCount=document.getElementById("guest-count");
  
+let count = 0;
+function incrementOne(){
+count +=1
+}
