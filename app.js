311da0bc537a305d21f39e23d671f2809ed18c20
Daina <79340025+dazeste@users.noreply.github.com>
2023-04-18 11:48:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const homeBtn = document.getElementById("home-htn");
 const guestBtn = document.getElementById("guest-btn");
 const countBtn = document.getElementById("count-btn");
-
+const homeCount = document.getElementById("home-count");
+const guestCount=document.getElementById("guest-count");
+ 
