8c73d36c865e0f98f4871bff6a318ae4bf9c8ada
Daina <79340025+dazeste@users.noreply.github.com>
2023-04-18 17:18:09 +0100

    html,javascript

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const homeBtn = document.getElementById("home-htn");
 const guestBtn = document.getElementById("guest-btn");
 const countBtn = document.getElementById("count-btn");
 const homeCount = document.getElementById("home-count");
-const guestCount=document.getElementById("guest-count");
+const guestCount = document.getElementById("guest-count");
+
+const homeOne = document.getElementById("home-one");
+const homeTwo = document.getElementById("home-two");
+const homeThree = document.getElementById("home-three");
+
+const guestOne = document.getElementById("guest-one");
+const guestTwo = document.getElementById("guest-two");
+const guestThree = document.getElementById("guest-three");
+
+
+
+
  
 let count = 0;
 
 function incrementOne(){
   count += 1
-  homeBtn.innerText = count;
-  guestBtn.innerText = count;
+  homeOne.innerText = count;
+  guestOne.innerText = count;
 }
