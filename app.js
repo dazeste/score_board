1be0941a425758cf858a3a8097da9a34afc378ad
Daina <79340025+dazeste@users.noreply.github.com>
2023-04-23 11:35:12 +0100

    html,js

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const homeBtn = document.getElementById("home-btn");
 const guestBtn = document.getElementById("guest-btn");
-const countBtn = document.getElementById("count-btn");
+// save btn
+const saveBtn = document.getElementById("save-btn");
 const homeCount = document.getElementById("home-count");
 const guestCount = document.getElementById("guest-count");
 
-// const homeOne = document.getElementById("home-one");
-// const homeTwo = document.getElementById("home-two");
-// const homeThree = document.getElementById("home-three");
 
-// const guestOne = document.getElementById("guest-one");
-// const guestTwo = document.getElementById("guest-two");
-// const guestThree = document.getElementById("guest-three");
 
 
 let countHome = 0;
